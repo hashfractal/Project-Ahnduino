beaf5152a1c2bf00ed5a0e78c7a019abf4fcacb0
Aldehyde <hsj3925@naver.com>
2022-04-12 21:42:03 +0900

    db access ref

---
+++ Reference/1112_hsj/실기확인용/StuManager.cs
@@ -0,0 +1,66 @@
+﻿using System    ;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1112_hsj
+{
+	/// <summary>
+	/// Member 관리 클래스 
+	/// </summary>
+	class StuManager
+	{
+	   #region 싱글톤 : 하나의 객체만 만들수 있는 클래스
+		private StuManager() { }
+
+		static StuManager()  
+		{
+		   
+			Singleton = new StuManager();
+		}
+		static public StuManager Singleton { get; private set; }
+
+		#endregion
+
+		WBDataBase db = new WBDataBase();
+			   
+		#region 기능메서드
+
+		public List<Student> PrintAll()
+		{
+			return db.PrintAll();
+		}
+
+		public bool InsertStudent(int number, string name, string subject, int grade)
+		{
+			try
+			{
+				Student stu = new Student(number, name, subject, grade);
+
+				return db.InsertStudent(stu);
+			}
+			catch(Exception)
+			{
+				return false;
+			}
+		}
+
+		public bool DeleteStudent(int number)
+		{
+			return db.DeleteStudent(number);
+		}
+
+		public Student NumberToStudent(int number)
+		{
+			return db.NumberToStudent(number);
+		}
+		
+		public bool UpdateStudent(int number, string type, int grade)
+		{
+			return db.UpdateStudent(number, type, grade);
+		}
+		#endregion
+
+	}
+}
---
+++ Reference/1112_hsj/실기확인용/StudentAddForm.Designer.cs
@@ -0,0 +1,176 @@
+﻿
+namespace _1112_hsj
+{
+	partial class StudentAddForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.button3 = new System.Windows.Forms.Button();
+			this.comboBox2 = new System.Windows.Forms.ComboBox();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.groupBox2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.button3);
+			this.groupBox2.Controls.Add(this.comboBox2);
+			this.groupBox2.Controls.Add(this.comboBox1);
+			this.groupBox2.Controls.Add(this.textBox2);
+			this.groupBox2.Controls.Add(this.textBox1);
+			this.groupBox2.Controls.Add(this.label5);
+			this.groupBox2.Controls.Add(this.label4);
+			this.groupBox2.Controls.Add(this.label3);
+			this.groupBox2.Controls.Add(this.label2);
+			this.groupBox2.Location = new System.Drawing.Point(23, 23);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(260, 265);
+			this.groupBox2.TabIndex = 4;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "[회원정보추가]";
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(150, 214);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(75, 23);
+			this.button3.TabIndex = 12;
+			this.button3.Text = "저장";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// comboBox2
+			// 
+			this.comboBox2.FormattingEnabled = true;
+			this.comboBox2.Items.AddRange(new object[] {
+			"1",
+			"2",
+			"3",
+			"4"});
+			this.comboBox2.Location = new System.Drawing.Point(71, 172);
+			this.comboBox2.Name = "comboBox2";
+			this.comboBox2.Size = new System.Drawing.Size(154, 20);
+			this.comboBox2.TabIndex = 9;
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Items.AddRange(new object[] {
+			"컴퓨터",
+			"IT",
+			"게임",
+			"기타"});
+			this.comboBox1.Location = new System.Drawing.Point(71, 126);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(154, 20);
+			this.comboBox1.TabIndex = 8;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(71, 86);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(154, 21);
+			this.textBox2.TabIndex = 7;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(71, 48);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(154, 21);
+			this.textBox1.TabIndex = 6;
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.Location = new System.Drawing.Point(21, 180);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(29, 12);
+			this.label5.TabIndex = 5;
+			this.label5.Text = "학년";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(21, 135);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(29, 12);
+			this.label4.TabIndex = 4;
+			this.label4.Text = "학과";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(21, 95);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(29, 12);
+			this.label3.TabIndex = 3;
+			this.label3.Text = "이름";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(21, 57);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(29, 12);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "학번";
+			// 
+			// StudentAddForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(308, 307);
+			this.Controls.Add(this.groupBox2);
+			this.Name = "StudentAddForm";
+			this.Text = "StudentAddForm";
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox2.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.ComboBox comboBox2;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+	}
+}
