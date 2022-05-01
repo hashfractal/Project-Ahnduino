aebbff42000fb732ea19be5206f1926a0beb1ed2
hsj3925 <hsj3925@naver.com>
2022-05-01 16:16:16 +0900

    qwer

--- Desktop/Ahnduino/Ahnduino/MVVM/ViewModel/MainViewModel.cs
+++ Desktop/Ahnduino/Ahnduino/MVVM/ViewModel/MainViewModel.cs
@@ -9,9 +9,43 @@ namespace Ahnduino.MVVM.ViewModel
 {
 	internal class MainViewModel : ObservableObject
 	{
+
+		public RelayCommand HomeViewCommand { get; set; }
+		public RelayCommand DiscoveryViewCommand { get; set; }
+
+		public HomeViewModel HomeViewModel { get; set; }
+
+		public DiscoveryViewModel DiscoveryViewModel { get; set; }
+
+
+		private object? _currentView;
+
+		public object CurrentView
+		{
+			get { return _currentView !; } 
+			set
+			{
+				_currentView = value;
+				OnPropertyChanged();
+			} 
+
+		}
+
 		public MainViewModel()
 		{
+			HomeViewModel = new HomeViewModel();
+			DiscoveryViewModel = new DiscoveryViewModel();
+			CurrentView = HomeViewModel;
 
+			HomeViewCommand = new RelayCommand(o =>
+			{
+				CurrentView = HomeViewModel;
+			});
+
+			DiscoveryViewCommand = new RelayCommand(o =>
+			{
+				CurrentView = DiscoveryViewModel;
+			});
 		}
 	}
 }
